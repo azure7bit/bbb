542196aad456bbd1705c501c4019e953af557923
Bentang <bentang@41studio.com>
2013-12-18 23:38:54 +0700

    optimize production

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'act_as_countable'
-
 # Initialize the rails application
 BandungBangkitBersinar::Application.initialize!
