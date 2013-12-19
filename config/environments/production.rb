55cdec764c9a7f45fa4fd11f2f7fe84f58ac1d6e
Asep Saepudin <asevenfold7bit@gmail.com>
2013-12-19 20:54:05 +0700

    resolve asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ BandungBangkitBersinar::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += ['blueprint/screen.css', 'pdf.css', 'jquery.ui.datepicker.js', 'pdf.js']
+  config.assets.precompile += ['blueprint/screen.css', 'pdf.css', 'jquery.ui.datepicker.js', 'pdf.js', 'number_pages.js']
 
   # Generate digests for assets URLs
   config.assets.digest = true
