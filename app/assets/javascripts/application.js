53d3c2c06c5a898c51551bd22be27bce498293c2
Asep Saepudin <asevenfold7bit@gmail.com>
2013-12-21 16:57:22 +0700

    precompile js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,15 +55,3 @@
 //= require jpicker-1.1.6.min
 //= require jquery.jeditable.mini
 //= require_tree .
-
-function remove_fields(link) {
-    $(link).prev("input[type=hidden]").val("1");
-    $(link).closest(".fields").hide();
-}
-
-function add_fields(link, association, content) {
-    var new_id = new Date().getTime();
-    var regex = new RegExp("new_" + association, "g");
-    $(link).parent().after(content.replace(regex, new_id));
-    $('#new-item-fields').modal('show');
-}
