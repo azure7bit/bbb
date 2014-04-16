e0be976ac926aec8cddafc7698c1b19a85965702
Asep Saepudin <asevenfold7bit@gmail.com>
2014-04-17 01:10:11 +0700

    assets files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ BandungBangkitBersinar::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
