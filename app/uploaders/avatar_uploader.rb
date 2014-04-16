428e1acadc5da1da464d11cde4f348122c8b4501
Asep Saepudin <asevenfold7bit@gmail.com>
2014-04-16 19:14:50 +0700

    fixed minimagick

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
   include Sprockets::Helpers::RailsHelper
 
   # Choose what kind of storage to use for this uploader:
