5bfdd67519c41d0415336d07230d5b23f8618ce7
Asep Saepudin <asevenfold7bit@gmail.com>
2014-05-01 14:28:05 +0700

    remove test

--- test/functional/customer_payments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CustomerPaymentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/supplier_payment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SupplierPaymentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
