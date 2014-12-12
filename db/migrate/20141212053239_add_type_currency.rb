859b4cca5d37c7cf93ddf9371ed574aa813f4588
bentang <asevenfold7bit@gmail.com>
2014-12-12 13:58:13 +0700

    change migration

--- db/migrate/20141212053239_add_type_currency.rb
+++ db/migrate/20141212053239_add_type_currency.rb
@@ -1,6 +1,8 @@
 class AddTypeCurrency < ActiveRecord::Migration
   def up
-  	add_column :currency_types, :boolean, :default => false
+  	add_column :purchase_orders, :currency_type, :boolean, default: :false
+    add_column :po_receives, :currency_type, :boolean, default: :false
+    add_column :sales_invoices, :currency_type, :boolean, default: :false
   end
 
   def down
